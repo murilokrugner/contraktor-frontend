bb8826a122501efe54ddf35e831302b25fd14f68
murilokrugner <murilokrugner@hotmail.com>
2020-06-29 22:15:41 -0300

    ...

--- src/pages/NewContracts/index.js
+++ src/pages/NewContracts/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from '../../components/Header';
-import { Form, Input, FileInput } from '@rocketseat/unform';
+import { Form, Input, FileInput, useField } from '@rocketseat/unform';
 import { Link } from 'react-router-dom';
 import * as Yup from 'yup';
 import { toast } from 'react-toastify';
@@ -14,48 +14,66 @@ const schema = Yup.object().shape({
   expirydate: Yup.string().required('A data final é obrigatória'),
 });
 
-export default function NewContracts() {
-  function handleSubmit(data) {
+export default class NewContracts extends Component {
+  state = {
+    uploadedFiles: [],
+  };
+
+  handleUpload = files => {
+    console.log(files);
+  };
+
+  handleSubmit = data => {
     const response = api.post('/contracts', {
-      data,
+      title: data.title,
+      initialdate: data.initialdate,
+      expirydate: data.expirydate,
     });
-    console.log(data);
-    if (response) {
+    const responsefile = api.post('/files', {
+      file: data,
+    });
+
+    if (responsefile) {
       toast.success('Contrato cadastrado!');
     } else {
       toast.error('Não foi possível cadastrar o contrato!');
     }
+
+    console.log(data.file);
+  };
+
+  render() {
+    return (
+      <Container>
+        <Header />
+        <Form schema={schema} >
+          <h3>Novo contrato</h3>
+          <Input name="title" placeholder="Digite o titulo do contrato" />
+          <Input
+            name="initialdate"
+            type="text"
+            format="dd/MM/yyyy"
+            placeholder="Data inicial do contrato - dd/mmm/yyyy"
+          />
+          <Input
+            name="expirydate"
+            type="text"
+            placeholder="Data final do contrato - dd/mmm/yyyy"
+          />
+
+          <File>
+            <label htmlFor="file">
+              <img src={uploadIcon} alt="file" />
+              <input type="file" id="file" />
+            </label>
+          </File>
+
+          <button type="submit">Cadastrar</button>
+          <Link to="/contracts">
+            <button type="button">Cancelar</button>
+          </Link>
+        </Form>
+      </Container>
+    );
   }
-
-  return (
-    <Container>
-      <Header />
-      <Form schema={schema} onSubmit={handleSubmit}>
-        <h3>Novo contrato</h3>
-        <Input name="title" placeholder="Digite o titulo do contrato" />
-        <Input
-          name="initialdate"
-          type="text"
-          format="dd/MM/yyyy"
-          placeholder="Data inicial do contrato - dd/mmm/yyyy"
-        />
-        <Input
-          name="expirydate"
-          type="text"
-          placeholder="Data final do contrato - dd/mmm/yyyy"
-        />
-        <File>
-          <label htmlFor="file">
-            <img src={uploadIcon} alt="file" />
-            <Input name="file" type="file" id="file" accept="pdf/*" />
-          </label>
-        </File>
-
-        <button type="submit">Cadastrar</button>
-        <Link to="/contracts">
-          <button type="button">Cancelar</button>
-        </Link>
-      </Form>
-    </Container>
-  );
 }
